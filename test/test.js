b6bc6c4023607b9c8f9702660d059791a229e977
Tonye Jack <jtonye@ymail.com>
2022-12-31 22:52:44 -0700

    Updated action

--- test/test.js
+++ test/test.js
@@ -1,28 +1,12 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  let args = ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'];
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: args,
-    ignoreDefaultArgs: ['--disable-extensions'],
-    executablePath: '/usr/bin/google-chrome',
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  try {
-    await page.goto('https://google.com', {
-      waitUntil: 'load',
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
-  try {
-    await page.pdf({path: 'test.pdf', format: 'a4'});
-  } catch (e) {
-    console.log(e);
-  }
+  await page.goto('https://news.ycombinator.com', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({ path: 'test.pdf', format: 'a4' });
 
   await browser.close();
 })();
